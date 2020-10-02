42955c3bf8e2214f41db63265d69dfd357ae2a2d
Vineeth-commits <63061830+Vineeth-commits@users.noreply.github.com>
2020-10-02 06:06:02 +0530

    changed the variable name

--- funcop.cpp
+++ funcop.cpp
@@ -2,14 +2,14 @@
 #include <iostream>
 using namespace std;
 
-int f( int i) {
+int f( int q) {
 	cout << "this is f()" << endl;
-	return i;
+	return q;
 }
 
 int main( int argc, char ** argv ) {
-	int i;
-	i = f(47);
-	printf("i is %d\n", i);
+	int n;
+	n = f(47);
+	printf("n is %d\n", n);
 	return 0;
 }
